2487aa38772d4fdbc145765ee62c6b6dbea6bc47
Felipe Bueno <felipe@MacBook-Air-de-Felipe.local>
2026-04-18 12:40:40 -0300

    First commit

---
+++ src/main/java/com/github/fellyipe/assettrackapi/AssetTrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.fellyipe.assettrackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetTrackApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssetTrackApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/fellyipe/assettrackapi/AssetTrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fellyipe.assettrackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetTrackApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
